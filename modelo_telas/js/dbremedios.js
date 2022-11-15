5dfac4c975fd6d4417d1e53691d33eae2564f49d
sil-barbosa <ma.dosil82@gmail.com>
2022-11-15 16:23:10 -0300

    Aprimoramento da tela "medicamentos".
    Criado arquivo em json, código alterado no arquivo HTML para leitura do banco de dados, estilo da página modificado para abrigar layout responsivo.

---
+++ modelo_telas/js/dbremedios.js
@@ -0,0 +1,34 @@
+db = {
+    dados: [
+        {
+            nome: "Dipirona",
+            descricao: "Combate dor e febre",
+            dosagemRotina: "01 gota por quilo do paciente em caso de dor ou febre.",
+            ilustracao: "assets/dipirona.jpg",
+        },
+        {
+            nome: "Ferrovitan",
+            descricao: "Suplemento vitamínico/mineral",
+            dosagemRotina: "Duas gotas por dia até os dois anos de idade.",
+            ilustracao: "assets/ferrovitan.jpg",
+        },
+        {
+            nome: "Vitamina D",
+            descricao: "Vitamina",
+            dosagemRotina: "Duas gotas por dia até os dois anos de idade.",
+            ilustracao: "assets/vitamina d.jpg",
+        },
+        {
+            nome: "Dipropionato de Beclometasona",
+            descricao: "Prevenção da rinite",
+            dosagemRotina: "Dois jatos em caso de crise.",
+            ilustracao: "assets/dipropionato beclometasona.jpg",
+        },
+        {
+            nome: "Sabultamol",
+            descricao: "Remédio contra bronquite",
+            dosagemRotina: "5ml pela manhã e 5ml de noite durante 08 dias.",
+            ilustracao: "assets/sabultamol.jpg",
+        }
+    ]
+}
